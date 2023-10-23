a63ac0e39be0fe036ad734ceb5a1d311cb5efdfa
Antonia Oancea <92017076+AntoniaOancea@users.noreply.github.com>
2023-10-23 20:17:49 +0300

    final-homework2

---
+++ Homework 2/homework_2/homework_2.ino
@@ -0,0 +1,50 @@
+const int redPotPin = A2;
+const int greenPotPin = A1;
+const int bluePotPin = A0;
+
+const int redLedPin = 11;
+const int greenLedPin = 10;
+const int blueLedPin = 9;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+const float maxPotValue = 1023.0;
+const float maxValue = 255.0;
+const float minPotValue = 0.0;
+const float minValue = 0.0;
+
+void setup() {
+  pinMode(redPotPin, INPUT);
+  pinMode(greenPotPin, INPUT);
+  pinMode(bluePotPin, INPUT);
+
+  pinMode(redLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  redValue = analogRead(redPotPin);
+  greenValue = analogRead(greenPotPin);
+  blueValue = analogRead(bluePotPin);
+
+  redValue = map(redValue, minPotValue, maxPotValue, minValue, maxValue);
+  greenValue = map(greenValue, minPotValue, maxPotValue, minValue, maxValue);
+  blueValue = map(blueValue, minPotValue, maxPotValue, minValue, maxValue);
+
+  Serial.print("red = ");
+  Serial.println(redValue);
+  analogWrite(redLedPin, redValue);
+
+  Serial.print("green = ");
+  Serial.println(greenValue);
+  analogWrite(greenLedPin, greenValue);
+
+  Serial.print("blue = ");
+  Serial.println(blueValue);
+  analogWrite(blueLedPin, blueValue);
+}
